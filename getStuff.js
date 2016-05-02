5c80d5866aaad5904c50d540c55405287b677a8c
redhendery <redhendery@gmail.com>
2016-05-02 15:25:10 +1200

    prints file as undefined. Giving up to do something less annoying.

--- getStuff.js
+++ getStuff.js
@@ -3,7 +3,14 @@ var request = require('superagent');
 var getStuff = function(path, callback) {
   request('http://www.stuff.co.nz', function(err, res) {
     callback(null, res.text);
-  });
+      if(err) {
+        callback(err);
+      }
+      else {
+        callback(null, res);
+        }
+      }
+  );
 };
 
 module.exports = getStuff;
